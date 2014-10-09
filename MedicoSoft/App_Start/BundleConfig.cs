cb48f6583c29da8f571ecd6680f3bcc7c4dd94fe
eliCuippi <bijoux78740@hotmail.fr>
2014-10-09 16:40:15 +0200

    premier batch

--- MedicoSoft/App_Start/BundleConfig.cs
+++ MedicoSoft/App_Start/BundleConfig.cs
@@ -21,7 +21,7 @@ namespace MedicoSoft
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css", "~/Content/bootstrap-theme.min.css",
                       "~/Content/site.css"));
         }
     }
