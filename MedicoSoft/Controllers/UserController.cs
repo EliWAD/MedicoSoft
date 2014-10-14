ef2ad3c76ab0d37a9b6ffe965d202cb11caf1286
eliCuippi <bijoux78740@hotmail.fr>
2014-10-14 16:34:14 +0200

    ajout de la liste des utilisateurs

--- MedicoSoft/Controllers/UserController.cs
+++ MedicoSoft/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using MedicoSoft.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,17 +14,22 @@ namespace MedicoSoft.Controllers
         // GET: /User/
         public ActionResult Index()
         {
-            return View();
+            if(MySession.Login == null)
+            return RedirectToRoute(new{ controller = "Login" , action ="Form"});          
+            
+            List<Utilisateur> maListe = Utilisateur.getInfos();
+            //MySession.isMedecin = MySession.User.getRole() == TypeOfUser.Medecin;
         }
 
-        public ActionResult Liste()
+        public ActionResult Liste(Utilisateur u)
         {
-            return View();
+
+
+            Medecin me = MySession.User.ConvertMedecin();
+            return RedirectToRoute(new { controller = "Login", action = "Form" });
+            
         }
 
-        public ActionResult FichePatient()
-        {
-            return View();
-        }
+        
 	}
 }
--- MedicoSoft/Models/MySession.cs
+++ MedicoSoft/Models/MySession.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,31 @@ namespace MedicoSoft.Models
     {
         public static string Login
         {
-            get{ return HttpContext.Current.Session["Login"].ToString();}
-            set{ HttpContext.Current.Session["Login"].ToString();}
+            get{
+                try
+                {
+                    return HttpContext.Current.Session["Login"].ToString();
+
+                }
+                catch (Exception)
+                {
+                    return null;
+                } 
+            }
+            set{ HttpContext.Current.Session["Login"]= value;}
+        }
+
+        public static Utilisateur User
+        {
+            get { return (Utilisateur)HttpContext.Current.Session["User"]; }
+            set { HttpContext.Current.Session["User"] = value; }
+
+        }
+
+        public static bool isMedecin
+        {
+            get { return (bool)HttpContext.Current.Session["isMedecin"]; }
+            set { HttpContext.Current.Session["isMedecin"] = value; }
         }
     }
 }
