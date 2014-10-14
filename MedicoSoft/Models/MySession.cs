1e65dac3ede55c245f6977c966218c0f7129b4e5
eliCuippi <bijoux78740@hotmail.fr>
2014-10-14 11:24:52 +0200

    ajout MySession

---
+++ MedicoSoft/Models/MySession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicoSoft.Models
+{
+    //equivalent à utils.cs
+    public static class MySession
+    {
+        public static string Login
+        {
+            get{ return HttpContext.Current.Session["Login"].ToString();}
+            set{ HttpContext.Current.Session["Login"].ToString();}
+        }
+    }
+}
